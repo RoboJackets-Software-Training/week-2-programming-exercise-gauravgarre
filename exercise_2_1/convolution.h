a6961422fc750c3e4b5d8bc22ec7c51c118bc5d6
vagrant <gauravgarre@gmail.com>
2020-09-30 22:16:04 +0000

    Completed exercise 2.1

---
+++ exercise_2_1/convolution.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+#pragma once
+
+std::vector<double> applyConvolution(std::vector<double> x, std::vector<double> w, bool pack_with_zeros);
+void printVector(std::vector<double> vec);
--- exercise_2_1/exercise_2_1.cpp
+++ exercise_2_1/exercise_2_1.cpp
@@ -1,5 +1,41 @@
+#include "convolution.h"
+#include <iostream>
 #include <vector>
 
-int main() {
+std::vector<double> readInVector(std::string s) {
+  int prev_location = 0;
+  int next_location = 0;
+  std::vector<double> result;
+  while(s.find(',', prev_location) != std::string::npos) {
+    next_location = s.find(',', prev_location);
+    result.push_back(std::stod(s.substr(prev_location, next_location - prev_location)));
+    next_location++;
+    prev_location = next_location;
+  }
+  result.push_back(std::stod(s.substr(prev_location, std::string::npos)));
+  return result;
+}
 
+int main() {
+  std::vector<double> x;
+  std::vector<double> w;
+  bool pack_with_zeros = true;
+
+  std::string s;
+  std::cin >> s;
+  if(s == "false") {
+    pack_with_zeros = false;
+  }
+  std::cin >> s;
+  x = readInVector(s);
+  std::cin >> s;
+  w = readInVector(s);
+
+  applyConvolution(x, w, pack_with_zeros);
+
+  pack_with_zeros = false;
+
+  applyConvolution(x, w, pack_with_zeros);
+
+  return 0;
 }
